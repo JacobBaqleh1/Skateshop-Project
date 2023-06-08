ab99a53594cd6e83df4ae22c6dcf21bfc2e59b30
Jacob Baqleh <baqlehjacob1@gmail.com>
2023-06-07 19:38:23 -0700

    update

---
+++ src/pages/contact.jsx
@@ -0,0 +1,21 @@
+export const Contact = () => {
+  return (
+    <div>
+      <h1>
+        Thank you for choosing our company! We are here to assist you with any
+        questions or inquiries you may have.
+      </h1>{" "}
+      <h2>
+        Please refer to the following contact information for specific
+        inquiries:
+      </h2>
+      <p>
+        {" "}
+        If you have any questions regarding shipping, product inquiries,billing,
+        or Technical Support please reach out to our dedicated team at
+        1(800)887-223 or shredshop@gmale.com They will be more than happy to
+        assist you with any related queries.
+      </p>
+    </div>
+  );
+};
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,20 @@
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Shredders Skateshop </h1>
+        <h5>Shredding since 1995</h5>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
