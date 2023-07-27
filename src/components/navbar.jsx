8cc148590f6c33afec5a4c0dc01e39763c69c89a
Jacob Baqleh <baqlehjacob1@gmail.com>
2023-07-26 20:28:46 -0700

    added mailing list text

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,17 +5,32 @@ import "./navbar.css";
 
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="links">
-        <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
+    <div className="container">
+      <div className="navbar">
+        <div className="links">
+          <Link to="/"> Shop </Link>
+          <Link to="/contact"> Contact </Link>
 
-        <Link to="/wishlist">
-          <Heart size={32} />
-        </Link>
-        <Link to="/cart">
-          <ShoppingCart size={32} />
-        </Link>
+          <Link to="/wishlist">
+            <Heart size={32} />
+          </Link>
+          <Link to="/cart">
+            <ShoppingCart size={32} />
+          </Link>
+        </div>
+      </div>
+      <div className="secondNavbar">
+        <div className="links">
+          <Link to="/"> Shop </Link>
+          <Link to="/contact"> Contact </Link>
+
+          <Link to="/wishlist">
+            <Heart size={32} />
+          </Link>
+          <Link to="/cart">
+            <ShoppingCart size={32} />
+          </Link>
+        </div>
       </div>
     </div>
   );
